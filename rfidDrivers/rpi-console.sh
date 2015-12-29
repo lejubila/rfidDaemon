74a3fc4953753e481d92dc9db8d672e883a4442e
David Bigagli <david.bigagli@gmail.com>
2015-12-29 18:49:53 +0100

    Rinominato file e variato descrizioni

--- rfidDrivers/rpi-console.sh
+++ rfidDrivers/rpi-console.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# Driver per lettura da rfid reader in emulazione di tastiera.
+# Driver per lettura da rfid reader collegato in emulazione di tastiera su Raspberry Pi.
 # Il codice viene letto da /dev/console. Per un corretto funzionamento devono essere diasbilitati
-# in /etc/inittab i runlevel relativi alle console ttyx che eseguono getty
+# in /etc/inittab i runlevel relativi alle console ttyx che eseguono getty.
+# E' stato testato su Raspberry Pi ver B
 #
 if [ -z $1 ]; then
     echo `sudo $0 run`
